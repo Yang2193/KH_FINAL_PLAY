9d3e797939ec739b2468df48990ec12569c148e3
thkim1230 <thkim1347@gmail.com>
2023-06-07 22:02:07 +0900

    2023.06.07/ 지도 api 추가

--- src/components/playPage/Map.js
+++ src/components/playPage/Map.js
@@ -8,12 +8,26 @@ const MapCss =styled.div`
     align-items: center;
 `
 const Map = () =>{
+    const{kakao} = window;
+    useEffect(()=>{
+        const container = document.getElementById('map');
+        const options = {
+            center : new kakao.maps.LatLng(33.450701,126.570667),
+            level:3
+        };
+        const map = new kakao.maps.Map(container,options);        
+    },[])
+
+
+    
 
     return(
-        <MapCss>
-            <div id="map" style="width:500px;height:400px;"></div>
-        </MapCss>
-    )
+        <>
+            <p>장소 : 불러올 데이터</p>
+            <p>주소 : 불러올 데이터</p>
+            <div id ="map" style={{width:'500px',height:'500px',margin : '50px'}}></div>
+        </>
+        )
 }
 
 export default Map;
