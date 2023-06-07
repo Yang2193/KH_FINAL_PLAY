8eb4287d8f8d28e2fa469695c7b5f434c36e1429
thkim <thkim1347@gmail.com>
2023-06-07 15:18:16 +0900

    2023/06/07 연극페이지 css 수정

--- src/components/playPage/Map.js
+++ src/components/playPage/Map.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { useEffect } from "react";
+
 const MapCss =styled.div`
     display: flex;
     justify-content: center;
@@ -9,7 +11,7 @@ const Map = () =>{
 
     return(
         <MapCss>
-            <p>대충 지도 api</p>
+            <div id="map" style="width:500px;height:400px;"></div>
         </MapCss>
     )
 }
--- src/pages/PlayPage.js
+++ src/pages/PlayPage.js
@@ -6,9 +6,18 @@ import Map from "../components/playPage/Map";
 import { useState } from "react";
 import Detail from "../components/playPage/Detail";
 import Info from "../components/playPage/Info";
+import styled from "styled-components";
+
+const Contents = styled.div`
+    width: 60%;
+    position: relative;
+    left: 20%;
+`
+
+
 const PlayPage = () => {
 
-    const[type,setType] = useState("");
+    const[type,setType] = useState("default");
 
 	const handleType = (e) =>{
 		setType(e);
@@ -17,14 +26,16 @@ const PlayPage = () => {
     return(
         <>
             <Header/>
-            <Info/>
-            <PlayNav  handleType={handleType}/>
-            {type === "default" &&(
-                <Detail/>
-            )}
-            {type === "map" &&(
-                <Map/>
-            )}
+            <Contents>
+                <Info/>
+                <PlayNav  handleType={handleType}/>
+                {type === "default" &&(
+                    <Detail/>
+                )}
+                {type === "map" &&(
+                    <Map/>
+                )}
+            </Contents>
             <Footer/>
         </>
     )
