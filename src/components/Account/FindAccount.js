a33d90c6b297ac1fa8566f46aecdc11528dc36b5
Chosh93 <chosh930911@gmail.com>
2023-06-09 21:17:59 +0900

    FindAccountFront

--- src/components/Account/FindAccount.js
+++ src/components/Account/FindAccount.js
@@ -4,95 +4,93 @@ import '../../styles/FindAccount.css';
 const FindUserIdPw = () => {
 
     // 아이디 패스워드 찾기 키보드 입력
-    const [userName, setUserName] = useState("");
-    const [userEmail, setUserEmail] = useState("");
-    const [userId, setUserId] = useState("");
+    const [idUserName, setIdUserName] = useState("");
+    const [idUserEmail, setIdUserEmail] = useState("");
+    const [pwUserId, setPwUserId] = useState("");
+    const [pwUserName, setPwUserName] = useState("");
+    const [pwUserEmail, setPwUserEmail] = useState("");
 
     // 유효성 검사
     const [isId, setIsId] = useState(false);
     const [isEmail, setIsEmail] = useState(false);
     const [isName, setIsName] = useState(false);
 
-    const onChangeUserId = (e) => {
-        const idRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{3,15}$/ // 아이디 정규표현식(3자리이상 15자리이내)
-        const idNow = e.target.value; 
-        setUserId(idNow);
-        if(!idRegex.test(idNow)) {
-            setIsId(false)
-        } else {
-            setIsId(true);
-        }
+    const onChangeIdUserName = (e) => {
+        const idUserNameNow = e.target.value;
+        setIdUserName(idUserNameNow);
     }
 
-    const onChangeUserName = (e) => {
-        const nameNow = e.target.value;
-        setUserName(nameNow); 
-        if(nameNow.length < 2 || nameNow.length > 10) {
-            setIsName(false);
-        } else {
-            setIsName(true);
-        }
+    const onChangeIdUserEmail = (e) => {
+        const idUserEmailNow = e.target.value;
+        setIdUserEmail(idUserEmailNow);
     }
 
-    const onChangeUserEmail = (e) => {
-        const emailRegex = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/ // 이메일 정규표현식
-        const emailNow = e.target.value ;
-        setUserEmail(emailNow);
+    const onChangePwUserId = (e) => {
+        const pwUserIdNow = e.target.value;
+        setPwUserId(pwUserIdNow);
+    }
 
-        if (!emailRegex.test(emailNow)) {
-            setIsEmail(false)
-        } else {
-            setIsEmail(true);
-        }        
+    const onChangePwUserName = (e) => {
+        const pwUserNameNow = e.target.value;
+        setPwUserName(pwUserNameNow);
+    }
+
+    const onChangePwUserEmail = (e) => {
+        const pwUserEmailNow = e.target.value;
+        setPwUserEmail(pwUserEmailNow);
     }
 
     // 이름과 메일 입력 값을 axios로 회원조회 접근 코드
 
     return (
         <>
-        <div className="container-find">
+        <h2>아이디 / 패스워드 찾기</h2>
+        <div className="find">
         {/* FORM SECTION */}
-        <div className="row">
+        <div className="find-section">
+        <div className="section-id"><h3>아이디</h3></div>
+        <div className="section-pw"><h3>패스워드</h3></div>
+        </div>
+            <div className="find-form">
             {/* FIND ID */}
-            <div className="col align-items-center flex-col">
-            <div className="form-wrapper align-items-center">
-                <div className="form">
-                <div className="input-group">
-                    <i className="bx bxs-user"></i>
-                    <input type="text" placeholder="User Name" value={userName} onChange={onChangeUserName} />
+                <div className="id-container">
+                    <div className="find-id">
+                        <div>
+                            <i></i>
+                            <input type="text" placeholder="User Name" value={idUserName} onChange={onChangeIdUserName} />
+                        </div>
+                        <div>
+                            <i></i>
+                            <input type="text" placeholder="User Email" value={idUserEmail} onChange={onChangeIdUserEmail} />
+                        </div>
+                        <button>아이디 찾기</button>
+                    </div>
                 </div>
-                <div className="input-group">
-                    <i className="bx bxs-lock-alt"></i>
-                    <input type="text" placeholder="User Email" value={userEmail} onChange={onChangeUserEmail} />
-                </div>
-                <button>아이디 찾기</button>
-                </div>
-            </div>
-            <div className="form-wrapper"></div>
-            </div>
             {/* FIND ID */}
             {/* FIND PW */}
-            <div className="col align-items-center flex-col">
-            <div className="align-items-center">
-                <div className="form">
-                <div className="input-group">
-                    <i className="bx bxs-user"></i>
-                    <input type="text" placeholder="User ID" value={userId} onChange={onChangeUserId}/>
+                <div className="pw-container">
+                    <div className="find-pw">
+                        <div>
+                            <div>
+                                <i></i>
+                                <input type="text" placeholder="User ID" value={pwUserId} onChange={onChangePwUserId}/>
+                            </div>
+                            <div>
+                                <i></i>
+                                <input type="text" placeholder="User name" value={pwUserName} onChange={onChangePwUserName}/>
+                            </div>
+                            <div>
+                                <i className="bx bxs-mail-send"></i>
+                                <input type="email" placeholder="Email" value={pwUserEmail} onChange={onChangePwUserEmail}/>
+                                <div>
+                                <button>인증</button>
+                                <button>패스워드 찾기</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="input-group">
-                    <i className="bx bxs-lock-alt"></i>
-                    <input type="text" placeholder="User name" value={userName} onChange={onChangeUserName}/>
-                </div>
-                <div className="input-group">
-                    <i className="bx bxs-mail-send"></i>
-                    <input type="email" placeholder="Email" value={userEmail} onChange={onChangeUserEmail}/>
-                    <div><button>인증</button></div>
-                </div>
-                <button>아이디 찾기</button>
-                </div>
-            </div>
-        </div>
-        {/* END FIND PW */}
+            {/* END FIND PW */}
             </div>
         </div>
         </>
