17496bd010c49203e070738a03c64ea130558f70
Chosh <chosh930911@gmail.com>
2023-06-13 14:42:01 +0900

    메인페이지 헤더에 임시 로그인링크 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import styled from "styled-components";
-import { MdMenu } from "react-icons/md";
+import { MdMenu, MdLogin } from "react-icons/md";
 
 const Container = styled.div`
   width: 100%;
@@ -36,10 +37,25 @@ const MenuBurger = styled(MdMenu)`
         
     }
 `
+const LoginBox = styled(MdLogin)`
+  width: 50px;
+  height: 50px;
+  color: white;
+  position: absolute;
+  left: 16px;
+  top: 50%;
+  transform: translateY(-50%);
 
+  @media (max-width: 768px) {
+    width: 40px;
+    height: 40px;
+    left: 8px;
+  }
+`
 
 const Header = ({children}) => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleResize = () => {
@@ -58,9 +74,14 @@ const Header = ({children}) => {
 
   }
 
+  const onClickLoginIcon = () => {
+    navigate("/login");
+  }
+
     return(
         <Container>
           {isMobile ? <>{children}</> : <LogoBox>{children}</LogoBox>}         
+          <LoginBox onClick={onClickLoginIcon}/>
           <MenuBurger/>   
         </Container>
     );
