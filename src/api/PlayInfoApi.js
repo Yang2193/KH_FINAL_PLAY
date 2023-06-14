f4e6b53bdf483008d33e884a81af30d55c9087dd
thkim1230 <thkim1347@gmail.com>
2023-06-14 23:16:39 +0900

    2023.06.14/ 공연상세정보 api 불러오는 api 수정

--- src/api/PlayInfoApi.js
+++ src/api/PlayInfoApi.js
@@ -4,7 +4,10 @@ const KH_DOMAIN = "http://localhost:8111";
 
 const PlayInfoApi = {
     selectPlayInfo : async(mt20id) => {
-        return await axios.get(KH_DOMAIN + `/play/${mt20id}}`);
+        const playId = {
+            playId : mt20id
+        }
+        return await axios.get(KH_DOMAIN + `/play/${mt20id}`,playId);
     },
     // selectActor : async(mt20id)=> {
     //     return await axios.get(KH_DOMAIN + `/play/selectActor?id=${mt20id}`);
