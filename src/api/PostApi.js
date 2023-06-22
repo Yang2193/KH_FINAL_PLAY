d2fe9845afc995332d12798dc3c8a3a64c3c24f1
1235rtyu <ch07124@naver.com>
2023-06-22 10:52:29 +0900

    postapi

--- src/api/PostApi.js
+++ src/api/PostApi.js
@@ -3,18 +3,11 @@ import Functions from "../utils/Functions";
 const Posts = "http://localhost:8111"; // 백엔드 API 서버 주소
 
 const PostAPI = {
+
   // 게시물 목록 조회
   getAllPosts: async () => {
-    try {
-      console.log(Functions.getRefreshToken()); // 테스트 한다고 넣은 예시 입니다 지우세요 ㅋㅋ
-      Functions.setAuthorizationHeader(); // 예시로 넣어놨습니다. 나중에 지우세요.
-      return await axios.get(`${Posts}/post/select`);
-
-    } catch(error){
-      await Functions.handleApiError(error); // 테스트 한다고 넣은 예시 입니다 지우세요 ㅋㅋ
-      console.log("여기까지 실행 되는지 확인1"); // 테스트 한다고 넣은 예시 입니다 지우세요 ㅋㅋ
-      return await axios.get(`${Posts}/post/select`); // 테스트 한다고 넣은 예시 입니다 지우세요 ㅋㅋ
-    } 
+    Functions.setAuthorizationHeader();
+    return await axios.get(`${Posts}/post/select`);
   },
 
   // 게시물 상세 정보 조회
@@ -25,6 +18,7 @@ const PostAPI = {
 
   // 게시물 등록
   addPost: async (postTitle, postContent, postImageUrl, postCategory, userId) => {
+    try {
     const postData = {
       postTitle: postTitle,
       postContent: postContent,
@@ -33,8 +27,15 @@ const PostAPI = {
       userId: userId,
     };
 
+    Functions.setAuthorizationHeader();
+
     const response = await axios.post(`${Posts}/post/postUpload`, postData);
     return response.data;
+  } catch(error){
+    await Functions.handleApiError(error);
+    const response1 = await axios.post(`${Posts}/post/postUpload`);
+    return response1.data;
+  }
   },
 
   // 조회수 증가
@@ -43,17 +44,13 @@ const PostAPI = {
     return response.data;
   },
 
-// 댓글 생성
-
-createComment: async (postId, newComment) => {
-  const commentData = {
-    postId: postId,
-    ...newComment,
-  };
-
-  const response = await axios.post(`${Posts}/${postId}/comments`, commentData);
-  return response.data;
-},
+  // 댓글 생성
+  createComment: async (postId, newComment) => {
+    const response = await axios.post(`${Posts}/comments/${postId}`, newComment, {
+     
+    });
+    return response.data;
+  },
 };
 
 export default PostAPI;
