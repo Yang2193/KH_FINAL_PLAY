1ecccea9f27ed28b78fb6860f764cd3c3d527284
1235rtyu <ch07124@naver.com>
2023-06-22 10:51:14 +0900

    06 22 포스트랑 댓글페이지

--- src/pages/PostDetailPage.js
+++ src/pages/PostDetailPage.js
@@ -1,24 +1,29 @@
+
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import PostAPI from '../api/PostApi';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import CommentPage from './CommentPage';
 
 const Background = styled.div`
-  background-color: #E2E2E2;
-  height: 1000px;
+  background-color: #e2e2e2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
 `;
 
 const PostDetailWrapper = styled.div`
-  max-width: 940px;
-  max-height: 1800px;
-  margin: 0 auto;
+  width: 900px;
+  height: auto;
+  min-height: 890px;
+  margin: 30px;
   padding: 20px;
   background-color: white;
   border-radius: 20px;
-  position: relative;
-  top: 70px;
+  flex-grow: 1;
 `;
 
 const PostHeader = styled.div`
@@ -38,7 +43,7 @@ const PostInfo = styled.div`
   color: #888;
   font-size: 14px;
   height: 48px;
-  background-color: #F6F6F6;
+  background-color: #f6f6f6;
   padding: 0 20px;
   border-radius: 5px;
 `;
@@ -46,8 +51,8 @@ const PostInfo = styled.div`
 const PostInfoItem = styled.span`
   display: inline-block;
   margin-right: 10px;
-  color: ${props => props.userId ? '#000' : '#888'};
-  font-weight: ${props => props.userId ? 'bold' : 'normal'};
+  color: ${(props) => (props.userId ? '#000' : '#888')};
+  font-weight: ${(props) => (props.userId ? 'bold' : 'normal')};
 `;
 
 const PostImage = styled.div`
@@ -55,63 +60,24 @@ const PostImage = styled.div`
 `;
 
 const PostContent = styled.div`
-  line-height: 1.6;
+  font-size: 16px;
+  line-height: 1.5;
+  white-space: pre-line;
+  margin-bottom: 20px;
 `;
 
 const LoadingMessage = styled.div`
-  text-align: center;
   font-size: 16px;
-  color: #888;
-  margin-top: 40px;
+  margin-bottom: 20px;
 `;
 
-const CommentSection = styled.div`
-  margin-top: 30px;
-`;
-
-const CommentInput = styled.input`
-  width: 88%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-`;
-
-const CommentButton = styled.button`
-  margin-left: 15px;
-  padding: 10px 20px;
-  background-color: #888;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-`;
-
-const CommentList = styled.ul`
-  list-style: none;
-  padding: 0;
+const CommentWrapper = styled.div`
   margin-top: 20px;
-`;
-
-const CommentItem = styled.li`
-  margin-bottom: 10px;
-`;
-
-const CommentContent = styled.div`
-  font-size: 14px;
-  margin-bottom: 5px;
-`;
-
-const CommentDate = styled.span`
-  font-size: 12px;
-  color: #888;
 `;
 
 const PostDetailPage = () => {
   const { postId } = useParams();
   const [post, setPost] = useState(null);
-  const [comment, setComment] = useState('');
-  const [comments, setComments] = useState([]);
-  const userId = window.localStorage.getItem("isUserId");
 
   useEffect(() => {
     fetchPost();
@@ -121,95 +87,45 @@ const PostDetailPage = () => {
     try {
       const data = await PostAPI.getPostById(postId);
       setPost(data);
-      if (data.comments) {
-        setComments(data.comments);
-      } else {
-        setComments([]);
-      }
-      increaseViews(postId);
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const increaseViews = async (postId) => {
-    try {
-      await PostAPI.increasePostViews(postId);
-      setPost(prevPost => ({ ...prevPost, postViews: prevPost.postViews + 1 }));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const formatWriteDate = (date) => {
-    const formattedDate = new Date(date).toLocaleDateString('en-US');
-    return formattedDate;
-  };
-
-  const handleCommentChange = (e) => {
-    setComment(e.target.value);
-  };
-
-  const handleSubmitComment = async () => {
-    try {
-      const newComment = {
-        commentContent: comment,
-        commentDate: new Date(),
-        postId: post.id,
-      };
-      const response = await PostAPI.createComment(newComment.postId, newComment);
-      setComments([...comments, response]);
-      setComment('');
-      console.log(response);
     } catch (error) {
       console.log(error);
     }
   };
 
   if (!post) {
-    return <LoadingMessage>Loading...</LoadingMessage>;
+    return (
+      <>
+        <Header />
+        <Background>
+          <LoadingMessage>Loading...</LoadingMessage>
+        </Background>
+        <Footer />
+      </>
+    );
   }
 
   return (
     <>
-      <Header></Header>
+      <Header />
       <Background>
         <PostDetailWrapper>
           <PostHeader>
             <PostTitle>{post.postTitle}</PostTitle>
             <PostInfo>
-              <div>
-                <PostInfoItem userId>작성자: {post.memberInfo.userId}</PostInfoItem>
-                <PostInfoItem>작성 날짜: {formatWriteDate(post.postDate)}</PostInfoItem>
-              </div>
-              <PostInfoItem>조회수: {post.postViews}</PostInfoItem>
+              <PostInfoItem userId>{post.memberInfo.userId}</PostInfoItem>
+              <PostInfoItem>{post.postDate}</PostInfoItem>
             </PostInfo>
           </PostHeader>
           <PostImage>
-            <img src={post.postImageUrl} alt="Post" />
+            <img src={post.imageUrl} alt="Post" />
           </PostImage>
-          <PostContent>{post.postContent}</PostContent>
-          <CommentSection>
-            <CommentInput
-              type="text"
-              placeholder="댓글을 입력하세요..."
-              value={comment}
-              onChange={handleCommentChange}
-            />
-            <CommentButton onClick={handleSubmitComment}>댓글 작성</CommentButton>
-            <CommentList>
-              {comments.map((comment) => (
-                <CommentItem key={comment.id}>
-                  <CommentContent>{comment.commentContent}</CommentContent>
-                  <CommentDate>{formatWriteDate(comment.commentDate)}</CommentDate>
-                </CommentItem>
-              ))}
-            </CommentList>
-          </CommentSection>
+          <PostContent>{post.content}</PostContent>
+          <CommentWrapper>
+            <CommentPage postId={postId} />
+          </CommentWrapper>
         </PostDetailWrapper>
       </Background>
-      <Footer></Footer>
+      <Footer />
     </>
   );
 };
--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,3 +1,4 @@
+// Post.js
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
@@ -8,119 +9,62 @@ import '../pages/ReviewBoard.css';
 
 const Post = () => {
   const [posts, setPosts] = useState([]);
-  const [sortedPosts, setSortedPosts] = useState([]);
-  const [isDataFetched, setIsDataFetched] = useState(false);
-  const [currentPage, setCurrentPage] = useState(0);
-  const ITEMS_PAGE = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const rsp = await PostAPI.getAllPosts();
-        console.log(rsp);
-        if (rsp.status === 200) {
-          setPosts(rsp.data);
-          setSortedPosts(rsp.data);
-        } else {
-          console.log("데이터가 없거나 불러오기를 실패함");
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
+    fetchPosts();
   }, []);
 
-  const sortSortNo = () => {
-    setSortedPosts([...posts].sort((a, b) => b.id - a.id));
-  };
-
-  // 프론트에서 증가 시키는 이유
-  // posts 배열을 순회하면서 변경된 게시물의 조회수만 1 실시간으로 증가시키기 위해
-  const increaseViews = async (postId) => {
+  const fetchPosts = async () => {
     try {
-      await PostAPI.increasePostViews(postId);
+      const data = await PostAPI.getAllPosts();
+      setPosts(data);
     } catch (error) {
       console.log(error);
     }
   };
 
-  const formatWriteDate = (date) => {
-    const currentDate = new Date();
-    const writeDate = new Date(date);
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
-    if (isSameDay(currentDate, writeDate)) {
-      const formattedTime = writeDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-      return ` ${formattedTime}`;
-    } else {
-      const formattedDate = writeDate.toLocaleDateString('ko', options);
-      return formattedDate;
-    }
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
-
-  const handlePageClick = (selectedPage) => {
-    setCurrentPage(selectedPage.selected);
-  };
-
-  const isSameDay = (date1, date2) => {
-    return (
-      date1.getFullYear() === date2.getFullYear() &&
-      date1.getMonth() === date2.getMonth() &&
-      date1.getDate() === date2.getDate()
-    );
-  };
-
-  const pageCount = Math.ceil(sortedPosts.length / ITEMS_PAGE);
-  const offset = currentPage * ITEMS_PAGE;
-  const currentPageData = sortedPosts.slice(offset, offset + ITEMS_PAGE);
-  
 
   return (
     <>
       <Header />
       <div className="ReviewBoardWrapper">
-        <h2>리뷰 게시판</h2>
-        <div className="ButtonWrapper">
-          <Link to="/postUpload">
-            <button className="insert">등록하기</button>
-          </Link>
-          <button className="insert" onClick={sortSortNo}>
-            최신순
-          </button>
-        </div>
+        <h1>Post</h1>
         <table className="ReviewTable">
           <thead>
             <tr>
-              <th>리뷰 제목</th>
-              <th>설명</th>
-              <th>작성 날짜</th>
-              <th className="text_id">닉네임</th>
-              <th>조회수</th>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>작성일</th>
             </tr>
           </thead>
           <tbody>
-            {currentPageData.map((post) => (
+            {currentPosts.map((post) => (
               <tr className="ReviewItem" key={post.id}>
                 <td className="ReviewTitle">
-                  <Link
-                    to={`/post/select/${post.id}`}
-                    className="ReviewLink"
-                    onClick={() => increaseViews(post.id)}
-                  >
+                  <Link to={`/post/${post.id}`} className="ReviewLink">
                     {post.postTitle}
                   </Link>
                 </td>
-                <td className="Explaination2">{post.postContent}</td>
-                <td className="WriteDate">{formatWriteDate(post.postDate)}</td>
-                <td className="Id">{post.memberInfo ? post.memberInfo.userNickname : ''}</td>
-                <td className="Views">{post.postViews}</td>
+                <td>{post.memberInfo.userId}</td>
+                <td>{post.postDate}</td>
               </tr>
             ))}
           </tbody>
-
         </table>
-        {pageCount > 1 && <PageNation pageCount={pageCount} onPageChange={handlePageClick} />}
+        <PageNation
+          postsPerPage={postsPerPage}
+          totalPosts={posts.length}
+          paginate={paginate}
+        />
       </div>
       <Footer />
     </>
