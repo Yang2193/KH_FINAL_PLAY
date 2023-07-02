c3629e6b52df7ed72bc4c2397a830f710592c45e
1235rtyu <ch07124@naver.com>
2023-07-02 18:48:34 +0900

    0702신고

--- src/components/Comment/Report.js
+++ src/components/Comment/Report.js
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
+import PostAPI from '../../api/PostApi';
 
 import RtMenuIcon from './ReortIcon.png';
 
 const CommentMenu = styled.div`
   position: absolute;
   top: 10px;
-  right: -120px;
-  width: 120px;
+  right: -220px;
+  width: 220px;
   background-color: #fff;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
   display: ${props => (props.show ? 'block' : 'none')};
   flex-direction: column;
-  padding: 5px;
+  padding: 10px;
   border-radius: 5px;
   z-index: 999;
 `;
@@ -30,37 +31,74 @@ const CommentMenuItem = styled.div`
 `;
 
 const RtMenuImage = styled.img`
+  margin-left: 5px;
   cursor: pointer;
-  display: ${props => (props.showImage ? 'block' : 'none')};
 `;
 
-const RtMenu = ({ handleReport }) => {
+const ReportForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+`;
+
+const ReportLabel = styled.label`
+  margin-bottom: 5px;
+`;
+
+const ReportInput = styled.input`
+  padding: 5px;
+  margin-bottom: 10px;
+`;
+
+const ReportButton = styled.button`
+  padding: 5px 10px;
+  background-color: #555;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+`;
+
+const RtMenu = () => {
   const [showRtMenu, setShowRtMenu] = useState(false);
-  const [isCommentAuthor, setIsCommentAuthor] = useState(false);
-  const [showImage, setShowImage] = useState(false); // 로그인 사용자에게 이미지를 보여줄지 여부
-
+  const [reportReason, setReportReason] = useState('');
   const location = useLocation();
-  const { postId, commentAuthorId } = useParams();
-  const loggedInUserId = localStorage.getItem('userId');
-
-  useEffect(() => {
-    setIsCommentAuthor(commentAuthorId === loggedInUserId);
-  }, [commentAuthorId, loggedInUserId]);
+  const [postId, setPostId] = useState('');
 
   useEffect(() => {
     const postId = new URLSearchParams(location.search).get('postId');
     if (postId) {
-      window.localStorage.setItem('postId', postId);
+      setPostId(postId);
     }
   }, [location.search]);
 
-  useEffect(() => {
-    const loggedInUserId = localStorage.getItem('userId');
-    setShowImage(loggedInUserId !== null);
-  }, []);
+
+  const handleReport = async (commentId, reportReason, userId, postId) => {
+    try {
+      const response = await PostAPI.reportComment(commentId, reportReason, userId, postId);
+      console.log('Report:', response.data);
+    } catch (error) {
+    }
+  };
 
   const toggleRtMenu = () => {
     setShowRtMenu(!showRtMenu);
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const commentId = parseInt(window.localStorage.getItem('commentId'),10);
+    const userId = parseInt(window.localStorage.getItem('userId'), 10); 
+    handleReport(commentId, reportReason, userId, postId); 
+  
+    setShowRtMenu(false);
+    setReportReason('');
+  };
+  const handleInputChange = e => {
+    setReportReason(e.target.value);
+  };
+
+  const handleExampleClick = example => {
+    setReportReason(example);
   };
 
   return (
@@ -69,12 +107,29 @@ const RtMenu = ({ handleReport }) => {
         src={RtMenuIcon}
         alt="Rt 메뉴"
         onClick={toggleRtMenu}
-        style={{ marginTop: '3px', marginLeft: '', cursor: 'pointer', height: '18px' }}
-        showImage={showImage}
+        style={{ marginTop: '5px', marginLeft: '', cursor: 'pointer', height: '15px' }}
       />
 
       <CommentMenu show={showRtMenu}>
-        <CommentMenuItem onClick={handleReport}>신고하기</CommentMenuItem>
+        <CommentMenuItem>신고하기</CommentMenuItem>
+        <ReportForm onSubmit={handleSubmit}>
+          <ReportLabel>신고 사유:</ReportLabel>
+          <ReportInput type="text" value={reportReason} onChange={handleInputChange} />
+          <ReportLabel>신고 예시:</ReportLabel>
+          <CommentMenuItem onClick={() => handleExampleClick('욕설 및 혐오 표현 사용')}>
+            욕설 및 혐오 표현 사용
+          </CommentMenuItem>
+          <CommentMenuItem onClick={() => handleExampleClick('스팸 또는 광고')}>
+            스팸 또는 광고
+          </CommentMenuItem>
+          <CommentMenuItem onClick={() => handleExampleClick('폭력적인 콘텐츠')}>
+            폭력적인 콘텐츠
+          </CommentMenuItem>
+          <CommentMenuItem onClick={() => handleExampleClick('개인정보 유출')}>
+            개인정보 유출
+          </CommentMenuItem>
+          <ReportButton type="submit">신고</ReportButton>
+        </ReportForm>
       </CommentMenu>
     </>
   );
