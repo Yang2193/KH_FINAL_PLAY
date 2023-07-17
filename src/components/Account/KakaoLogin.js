dfc19b5b8a58ba7ab7eddf36ea38985831ee1fc5
Chosh <chosh930911@gmail.com>
2023-07-17 13:00:12 +0900

    0717 / 카카오 css 추가 app.js 라우터 변경

--- src/components/Account/KakaoLogin.js
+++ src/components/Account/KakaoLogin.js
@@ -1,7 +1,38 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import AccountApi from '../../api/AccountApi';
-import axios from 'axios';
+import styled, { keyframes } from 'styled-components';
+
+const loadingAnimation = keyframes`
+  0%,
+  100% {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  50% {
+    opacity: 1;
+    transform: scale(1.2);
+  }
+`;
+
+const LoadingContainer = styled.div`
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const LoadingSpan = styled.span`
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background-color: ${props => props.backgroundColor};
+  border-radius: 50%;
+  animation: ${loadingAnimation} 1s 0s linear infinite;
+  margin-right: 8px;
+`;
+
 const KakaoLogin = () => {
     const navigate = useNavigate();
     const location = useLocation();
@@ -21,10 +52,13 @@ const KakaoLogin = () => {
         }
         getAccessToken();
     };
-      
-
     return (
         <>
+        <LoadingContainer>
+            <LoadingSpan backgroundColor="#990A2C" />
+            <LoadingSpan backgroundColor="#990A2C" />
+            <LoadingSpan backgroundColor="#990A2C" />
+        </LoadingContainer>
         </>
     )
 }
